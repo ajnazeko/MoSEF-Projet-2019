ea2f959798f48cdc5d300b54406d68edd33bd19d
ajnazeko <Ajna.Zekovic@etu.univ-paris1.fr>
2019-11-28 17:45:19 +0100

    ajout du pattern

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #créerla var date
-date= $(date +'%d %B %Y')
+date=$(date +'%d %B %Y')
 echo 'Bienvenue' $USERNAME ',Nous sommes le' $date
 
 echo "Quel répertoire vous intéresse?"
 read directory
 
+#premier pattern
+
+ls $directory/$1
 
